a38bd727d78274cedccbc924dca2348d769fd43b
RDN (Mac) <desmond.ngueguin@gmail.com>
2022-09-02 02:21:55 +0200

    Created the full feedback list

---
+++ src/components/header.jsx
@@ -0,0 +1,28 @@
+// import React from 'react'
+import PropTypes from 'prop-types'
+
+function Header({text, bgColor, textColor}) {
+    const headerStyles = {backgroundColor:bgColor, color:textColor}
+    return (
+        <header style={headerStyles}>
+            <div className="container">
+            <h1> {text} </h1>
+            </div>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    text:'Feedback UI',
+    bgColor: 'violet',
+    textColor: 'darkGreen'
+
+}
+
+Header.propTypes = {
+    text: PropTypes.string,
+    bgColor: PropTypes.string,
+    textColor: PropTypes.string,
+} 
+
+export default Header;
